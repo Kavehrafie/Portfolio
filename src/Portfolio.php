70ab9c42b89454a474d2d6a3917e6d886b5ef946
root <root@DESKTOP-8V6ENGO.localdomain>
2021-05-02 00:26:34 -0500

    initial commit

---
+++ src/Portfolio.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace K1vehr1fie\Portfolio;
+
+
+class Portfolio
+{
+    public function greet(String $name) : String
+    {
+        return 'Hi ' . $name . '! How are you doing?';
+    }
+}
